c5ea3683365404cbc1a6109ffc6dd9083880cdc9
Brandon Perry <brandon_perry@rapid7.com>
2012-03-19 15:13:51 -0700

    add report example

---
+++ examples/nexpose_report.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require_relative '../lib/nexpose'
+
+
+host = '127.0.0.1'
+port = 3780
+user = "v4test"
+pass = "buynexpose"
+
+@nsc = Nexpose::Connection.new(host, user, pass, port)
+
+@nsc.login
+
+sites = @nsc.site_listing
+
+sites.each do |site|
+  p site[:site_id].to_s + ". " + site[:name]
+end
+
+site = gets
+
+templates = @nsc.report_template_listing
+
+templates.each do |template|
+  p template[:template_id]
+end
+
+#report = Nexpose::ReportConfig.new(@nsc)
+#report.set_name("Test" + Time.now.to_i.to_s)
+#report.set_template_id("audit-report")
+#report.addFilter("SiteFilter", site.to_i)
+#report.set_format("csv")
+
+report = Nexpose::ReportAdHoc.new(@nsc, 'audit-report', 'csv')
+report.addFilter('site', site.to_i)
+p report.generate
+
+#gets
+#report.saveReport()
+
+#url = nil
+#while not url
+#  url = @nsc.report_last(report.config_id)
+#  select(nil, nil, nil, 10)
+#end
+
+#p url
+#gets
+#data = @nsc.download(url)
+
+#p data
