b3cf31c1f81aecc5e3d0e8070cd7a93dd6ca12c6
Michael S. Daines <michael_daines@rapid7.com>
2012-09-04 22:00:31 -0700

    Mass formatting changes to remove tabs where they still exist.

--- lib/nexpose/misc.rb
+++ lib/nexpose/misc.rb
@@ -1,85 +1,85 @@
 module Nexpose
-	module NexposeAPI
-		include XMLUtils
+  module NexposeAPI
+    include XMLUtils
 
-		def device_delete(param)
-			r = execute(make_xml('DeviceDeleteRequest', {'device-id' => param}))
-			r.success
-		end
+    def device_delete(param)
+      r = execute(make_xml('DeviceDeleteRequest', {'device-id' => param}))
+      r.success
+    end
 
-		def asset_group_delete(connection, id, debug = false)
-			r = execute(make_xml('AssetGroupDeleteRequest', {'group-id' => param}))
-			r.success
-		end
+    def asset_group_delete(connection, id, debug = false)
+      r = execute(make_xml('AssetGroupDeleteRequest', {'group-id' => param}))
+      r.success
+    end
 
-		#-------------------------------------------------------------------------
-		# Returns all asset group information
-		#-------------------------------------------------------------------------
-		def asset_groups_listing()
-			r = execute(make_xml('AssetGroupListingRequest'))
+    #-------------------------------------------------------------------------
+    # Returns all asset group information
+    #-------------------------------------------------------------------------
+    def asset_groups_listing()
+      r = execute(make_xml('AssetGroupListingRequest'))
 
-			if r.success
-				res = []
-				r.res.elements.each('//AssetGroupSummary') do |group|
-					res << {
-						:asset_group_id => group.attributes['id'].to_i,
-						:name => group.attributes['name'].to_s,
-						:description => group.attributes['description'].to_s,
-						:risk_score => group.attributes['riskscore'].to_f,
-					}
-				end
-				res
-			else
-				false
-			end
-		end
+      if r.success
+        res = []
+        r.res.elements.each('//AssetGroupSummary') do |group|
+          res << {
+            :asset_group_id => group.attributes['id'].to_i,
+            :name => group.attributes['name'].to_s,
+            :description => group.attributes['description'].to_s,
+            :risk_score => group.attributes['riskscore'].to_f,
+          }
+        end
+        res
+      else
+        false
+      end
+    end
 
-		#-------------------------------------------------------------------------
-		# Returns an asset group configuration information for a specific group ID
-		#-------------------------------------------------------------------------
-		def asset_group_config(group_id)
-			r = execute(make_xml('AssetGroupConfigRequest', {'group-id' => group_id}))
+    #-------------------------------------------------------------------------
+    # Returns an asset group configuration information for a specific group ID
+    #-------------------------------------------------------------------------
+    def asset_group_config(group_id)
+      r = execute(make_xml('AssetGroupConfigRequest', {'group-id' => group_id}))
 
-			if r.success
-				res = []
-				r.res.elements.each('//Devices/device') do |device_info|
-					res << {
-						:device_id => device_info.attributes['id'].to_i,
-						:site_id => device_info.attributes['site-id'].to_i,
-						:address => device_info.attributes['address'].to_s,
-						:riskfactor => device_info.attributes['riskfactor'].to_f,
-					}
-				end
-				res
-			else
-				false
-			end
-		end
+      if r.success
+        res = []
+        r.res.elements.each('//Devices/device') do |device_info|
+          res << {
+            :device_id => device_info.attributes['id'].to_i,
+            :site_id => device_info.attributes['site-id'].to_i,
+            :address => device_info.attributes['address'].to_s,
+            :riskfactor => device_info.attributes['riskfactor'].to_f,
+          }
+        end
+        res
+      else
+        false
+      end
+    end
 
-		#
-		# Lists all the users for the NSC along with the user details.
-		#
-		def list_users
-			r = execute(make_xml('UserListingRequest'))
-			if r.success
-				res = []
-				r.res.elements.each('//UserSummary') do |user_summary|
-					res << {
-						:auth_source => user_summary.attributes['authSource'],
-						:auth_module => user_summary.attributes['authModule'],
-						:user_name => user_summary.attributes['userName'],
-						:full_name => user_summary.attributes['fullName'],
-						:email => user_summary.attributes['email'],
-						:is_admin => user_summary.attributes['isAdmin'].to_s.chomp.eql?('1'),
-						:is_disabled => user_summary.attributes['disabled'].to_s.chomp.eql?('1'),
-						:site_count => user_summary.attributes['siteCount'],
-						:group_count => user_summary.attributes['groupCount']
-					}
-				end
-				res
-			else
-				false
-			end
-		end
-	end
+    #
+    # Lists all the users for the NSC along with the user details.
+    #
+    def list_users
+      r = execute(make_xml('UserListingRequest'))
+      if r.success
+        res = []
+        r.res.elements.each('//UserSummary') do |user_summary|
+          res << {
+            :auth_source => user_summary.attributes['authSource'],
+            :auth_module => user_summary.attributes['authModule'],
+            :user_name => user_summary.attributes['userName'],
+            :full_name => user_summary.attributes['fullName'],
+            :email => user_summary.attributes['email'],
+            :is_admin => user_summary.attributes['isAdmin'].to_s.chomp.eql?('1'),
+            :is_disabled => user_summary.attributes['disabled'].to_s.chomp.eql?('1'),
+            :site_count => user_summary.attributes['siteCount'],
+            :group_count => user_summary.attributes['groupCount']
+          }
+        end
+        res
+      else
+        false
+      end
+    end
+  end
 end
--- lib/nexpose/util.rb
+++ lib/nexpose/util.rb
@@ -1,36 +1,36 @@
 module Nexpose
-	module Sanitize
-		def replace_entities(str)
-			ret = str.dup
-			ret.gsub!(/&/, "&amp;")
-			ret.gsub!(/'/, "&apos;")
-			ret.gsub!(/"/, "&quot;")
-			ret.gsub!(/</, "&lt;")
-			ret.gsub!(/>/, "&gt;")
-			ret
-		end
-	end
+  module Sanitize
+    def replace_entities(str)
+      ret = str.dup
+      ret.gsub!(/&/, "&amp;")
+      ret.gsub!(/'/, "&apos;")
+      ret.gsub!(/"/, "&quot;")
+      ret.gsub!(/</, "&lt;")
+      ret.gsub!(/>/, "&gt;")
+      ret
+    end
+  end
 
-	module XMLUtils
-		def parse_xml(xml)
-			::REXML::Document.new(xml.to_s)
-		end
+  module XMLUtils
+    def parse_xml(xml)
+      ::REXML::Document.new(xml.to_s)
+    end
 
-		def make_xml(name, opts={}, data='', append_session_id=true)
-			xml = REXML::Element.new(name)
-			if (@session_id and append_session_id)
-				xml.attributes['session-id'] = @session_id
-			end
+    def make_xml(name, opts={}, data='', append_session_id=true)
+      xml = REXML::Element.new(name)
+      if (@session_id and append_session_id)
+        xml.attributes['session-id'] = @session_id
+      end
 
-			opts.keys.each do |k|
-				if opts[k] != nil
-					xml.attributes[k] = "#{opts[k]}"
-				end
-			end
+      opts.keys.each do |k|
+        if opts[k] != nil
+          xml.attributes[k] = "#{opts[k]}"
+        end
+      end
 
-			xml.text = data
+      xml.text = data
 
-			xml
-		end
-	end
+      xml
+    end
+  end
 end
