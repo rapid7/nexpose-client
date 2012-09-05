1447ef0bbec12aa4ac841d0c1d4d9f48efa57a81
Michael S. Daines <michael_daines@rapid7.com>
2012-09-04 21:57:38 -0700

    Cleans out dead methods from root file.

--- lib/nexpose.rb
+++ lib/nexpose.rb
@@ -3,22 +3,22 @@
 #
 =begin
 
-Copyright (C) 2009-2011, Rapid7 LLC
+Copyright (C) 2009-2012, Rapid7 LLC
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
 
     * Redistributions of source code must retain the above copyright notice,
-	  this list of conditions and the following disclaimer.
+      this list of conditions and the following disclaimer.
 
     * Redistributions in binary form must reproduce the above copyright notice,
-	  this list of conditions and the following disclaimer in the documentation
-	  and/or other materials provided with the distribution.
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
 
     * Neither the name of Rapid7 LLC nor the names of its contributors
-	  may be used to endorse or promote products derived from this software
-	  without specific prior written permission.
+      may be used to endorse or promote products derived from this software
+      without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -43,8 +43,8 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #
 # WARNING! This code is still rough and going through substantive changes. While
-#          you can build tools using this library today, keep in mind that method
-#          names and parameters may change in the future.
+#          you can build tools using this library today, keep in mind that
+#          method names and parameters may change in the future.
 #
 
 require 'date'
@@ -73,61 +73,31 @@ require 'nexpose/common'
 
 module Nexpose
 
-	# TODO add
-	def self.site_device_scan(connection, site_id, device_array, host_array, debug = false)
+  # TODO add
+  def self.site_device_scan(connection, site_id, device_array, host_array, debug = false)
 
-		request_xml = '<SiteDevicesScanRequest session-id="' + connection.session_id.to_s + '" site-id="' + site_id.to_s + '">'
-		request_xml += '<Devices>'
-		device_array.each do |d|
-			request_xml += '<device id="' + d.to_s + '"/>'
-		end
-		request_xml += '</Devices>'
-		request_xml += '<Hosts>'
-		# The host array can only by single IP addresses for now. TODO: Expand to full API Spec.
-		host_array.each do |h|
-			request_xml += '<range from="' + h.to_s + '"/>'
-		end
-		request_xml += '</Hosts>'
-		request_xml += '</SiteDevicesScanRequest>'
+    request_xml = '<SiteDevicesScanRequest session-id="' + connection.session_id.to_s + '" site-id="' + site_id.to_s + '">'
+    request_xml += '<Devices>'
+    device_array.each do |d|
+      request_xml += '<device id="' + d.to_s + '"/>'
+    end
+    request_xml += '</Devices>'
+    request_xml += '<Hosts>'
+    # The host array can only by single IP addresses for now. TODO: Expand to full API Spec.
+    host_array.each do |h|
+      request_xml += '<range from="' + h.to_s + '"/>'
+    end
+    request_xml += '</Hosts>'
+    request_xml += '</SiteDevicesScanRequest>'
 
-		r = connection.execute(request_xml)
-		r.success ? {:engine_id => r.attributes['engine_id'], :scan_id => r.attributes['scan-id']} : nil
-	end
+    r = connection.execute(request_xml)
+    r.success ? {:engine_id => r.attributes['engine_id'], :scan_id => r.attributes['scan-id']} : nil
+  end
 
-	# === Description
-	# TODO
-	def self.getAttribute(attribute, xml)
-		value = ''
-		#@value = substr(substr(strstr(strstr(@xml,@attribute),'"'),1),0,strpos(substr(strstr(strstr(@xml,@attribute),'"'),1),'"'))
-		value
-	end
-
-	# === Description
-	# Returns an ISO 8601 formatted date/time stamp. All dates in Nexpose must use this format.
-	def self.get_iso_8601_date(int_date)
-		#@date_mod = date('Ymd\THis000', @int_date)
-		date_mod = ''
-		return date_mod
-	end
-
-	# ==== Description
-	# Echos the last XML API request and response for the specified object.  (Useful for debugging)
-	def self.printXML(object)
-		puts "request" + object.request_xml.to_s
-		puts "response is " + object.response_xml.to_s
-	end
-
-
-	def self.testa(ip, port, user, passwd)
-		nsc = Connection.new(ip, user, passwd, port)
-
-		nsc.login
-		site_listing = SiteListing.new(nsc)
-
-		site_listing.sites.each do |site|
-			puts "name is #{site.site_name}"
-			puts "id is #{site.id}"
-		end
-	end
-
+  # ==== Description
+  # Echos the last XML API request and response for the specified object.  (Useful for debugging)
+  def self.printXML(object)
+    puts "request" + object.request_xml.to_s
+    puts "response is " + object.response_xml.to_s
+  end
 end
