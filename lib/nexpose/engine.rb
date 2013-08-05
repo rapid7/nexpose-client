d0b0e4943c5b83985b2362fae9465563ade01d93
Michael S. Daines <michael_daines@rapid7.com>
2013-08-05 11:34:42 -0700

    Refactoring scan engine methods to design principles.

---
+++ lib/nexpose/engine.rb
@@ -0,0 +1,192 @@
+module Nexpose
+  module NexposeAPI
+    include XMLUtils
+
+    # Removes a scan engine from the list of available engines.
+    #
+    # @param [Fixnum] engine_id Unique ID of an existing engine to remove.
+    # @param [String] scope Whether the engine is global or silo scoped.
+    # @return [Boolean] true if engine successfully deleted.
+    #
+    def delete_engine(engine_id, scope = 'silo')
+      xml = make_xml('EngineDeleteRequest',
+                     {'engine-id' => engine_id, 'scope' => scope})
+      response = execute(xml, '1.2')
+      response.success
+    end
+
+    # Provide a list of current scan activities for a specific Scan Engine.
+    #
+    # @return [Array[ScanSummary]] Array of ScanSummary objects associated with
+    #   each active scan on the engine.
+    #
+    def engine_activity(engine_id)
+      xml = make_xml('EngineActivityRequest', {'engine-id' => engine_id})
+      r = execute(xml)
+      arr = []
+      if r.success
+        r.res.elements.each('//ScanSummary') do |scan_event|
+          arr << ScanSummary.parse(scan_event)
+        end
+      end
+      arr
+    end
+
+    # Retrieve a list of all Scan Engines managed by the Security Console.
+    #
+    # @return [Array[EngineSummary]] Array of EngineSummary objects associated with
+    #   each engine associated with this security console.
+    #
+    def list_engines
+      response = execute(make_xml('EngineListingRequest'))
+      arr = []
+      if response.success
+        response.res.elements.each('//EngineSummary') do |engine|
+          arr << EngineSummary.new(engine.attributes['id'].to_i,
+                                   engine.attributes['name'],
+                                   engine.attributes['address'],
+                                   engine.attributes['port'].to_i,
+                                   engine.attributes['status'])
+        end
+      end
+      arr
+    end
+
+    alias_method :engines, :list_engines
+  end
+
+  # Object representing the current details of a scan engine attached to the security console.
+  #
+  class EngineSummary
+
+    # A unique ID that identifies this scan engine.
+    attr_reader :id
+    # The name of this scan engine.
+    attr_reader :name
+    # The hostname or IP address of the engine.
+    attr_reader :address
+    # The port there the engine is listening.
+    attr_reader :port
+    # The engine status. One of: active|pending-auth|incompatible|not-responding|unknown
+    attr_reader :status
+    # A parameter that specifies whether the engine has a global
+    # or silo-specific scope.
+    attr_reader :scope
+
+    def initialize(id, name, address, port, status, scope = 'silo')
+      @id = id
+      @name = name
+      @address = address
+      @port = port
+      @status = status
+      @scope = scope
+    end
+  end
+
+  # Engine connnection to a Nexpose console.
+  #
+  class Engine
+
+    # Unique numeric identifier for the scan engine, assigned by the console
+    # in the order of creation.
+    attr_accessor :id
+    # The IP address or DNS name of a scan engine.
+    attr_accessor :address
+    # A name assigned to the scan engine by the security console.
+    attr_accessor :name
+    # The port on which the engine listens for requests from the security
+    # console.
+    attr_accessor :port
+    # Whether the engine has a global or silo-specific scope.
+    attr_accessor :scope
+    # Relative priority of a scan engine.
+    # One of: very-low, low, normal, high, very-high
+    attr_accessor :priority
+
+    # Sites to which the scan engine is assigned.
+    attr_accessor :sites
+
+    def initialize(address, name = nil, port = 40814)
+      @id = -1
+      @address = address
+      @name = name
+      @name ||= address
+      @port = port
+      @scope = 'silo'
+      @sites = []
+    end
+
+    def self.load(connection, id)
+      xml = '<EngineConfigRequest session-id="' + connection.session_id + '"'
+      xml << %Q{ engine-id="#{id}"}
+      xml << ' />'
+      r = connection.execute(xml, '1.2')
+
+      if r.success
+        r.res.elements.each('EngineConfigResponse/EngineConfig') do |config|
+          engine = Engine.new(config.attributes['address'],
+                              config.attributes['name'],
+                              config.attributes['port'])
+          engine.id = config.attributes['id']
+          engine.scope = config.attributes['scope'] if config.attributes['scope']
+          engine.priority = config.attributes['priority'] if config.attributes['priority']
+          config.elements.each('Site') do |site|
+            engine.sites << SiteSummary.new(site.attributes['id'], site.attributes['name'])
+          end
+          return engine
+        end
+      end
+      nil
+    end
+
+    # Assign a site to this scan engine.
+    #
+    # @param [Fixnum] site_id Unique numerical ID of the site.
+    #
+    def add_site(site_id)
+      sites << SiteSummary.new(site_id, nil)
+    end
+
+    def to_xml
+      xml = '<EngineConfig'
+      xml << %Q{ id="#{id}"}
+      xml << %Q{ address="#{address}"}
+      xml << %Q{ name="#{name}"}
+      xml << %Q{ port="#{port}"}
+      xml << %Q{ scope="#{scope}"} if scope
+      xml << %Q{ priority="#{priority}"} if priority
+      xml << '>'
+      sites.each do |site|
+        xml << %Q{<Site id="#{site.id}" />}
+      end
+      xml << '</EngineConfig>'
+      xml
+    end
+
+    # Save this engine configuration to the security console.
+    #
+    # @param [Connection] connection Connection to console where site exists.
+    # @return [Fixnum] ID assigned to the scan engine.
+    #
+    def save(connection)
+      xml = '<EngineSaveRequest session-id="' + connection.session_id + '">'
+      xml << to_xml
+      xml << '</EngineSaveRequest>'
+
+      r = connection.execute(xml, '1.2')
+      if r.success
+        r.res.elements.each('EngineSaveResponse/EngineConfig') do |v|
+          return @id = v.attributes['id']
+        end
+      end
+    end
+
+    # Delete this scan engine configuration from the security console.
+    #
+    # @param [Connection] connection Connection to console where site exists.
+    #
+    def delete(connection)
+      connection.delete_engine(@id, @scope)
+    end
+  end
+end
