b5d64628c8cc63761155e49b4f1e081e97e5654a
Michael S. Daines <michael_daines@rapid7.com>
2013-08-14 22:00:52 -0700

    Implements base ticketing features.

--- lib/nexpose/ticket.rb
+++ lib/nexpose/ticket.rb
@@ -1,34 +1,10 @@
 module Nexpose
 
-  module Ticket
-
-    module State
-      OPEN = 'O'
-      ASSIGNED = 'A'
-      MODIFIED = 'M'
-      FIXED = 'X'
-      PARTIAL = 'P'
-      REJECTED_FIX = 'R'
-      PRIORITIZED = 'Z'
-      NOT_REPRODUCIBLE = 'F'
-      NOT_ISSUE = 'I'
-      CLOSED = 'C'
-      UNKNOWN = 'U'
-    end
-
-    module Priority
-      LOW = 'low'
-      MODERATE = 'moderate'
-      NORMAL = 'normal'
-      HIGH = 'high'
-      CRITICAL = 'critical'
-    end
-  end
-
   module NexposeAPI
     include XMLUtils
 
     def list_tickets
+      # TODO Should take in filters as arguments.
       xml = make_xml('TicketListingRequest')
       r = execute(xml, '1.2')
       tickets = []
@@ -48,6 +24,7 @@ module Nexpose
     # @return [Boolean] Whether or not the ticket deletion succeeded.
     #
     def delete_ticket(ticket)
+      # TODO Take Ticket object, too, and pull out IDs.
       delete_tickets([ticket])
     end
 
@@ -57,94 +34,13 @@ module Nexpose
     # @return [Boolean] Whether or not the ticket deletions succeeded.
     #
     def delete_tickets(tickets)
+      # TODO Take Ticket objects, too, and pull out IDs.
       xml = make_xml('TicketDeleteRequest')
       tickets.each do |id|
         xml.add_element('Ticket', {'id' => id})
       end
 
       (execute xml, '1.2').success
-    end
-
-    # Create a Nexpose ticket
-    #
-    # ticket_info: A hash of the data to be used to create a ticket in Nexpose:
-    # :name        => The name of the ticket (Required)
-    # :device_id   => The Nexpose device ID for the device being ticketed (Required)
-    # :assigned_to => The Nexpose user to whom this ticket is assigned (Required)
-    # :priority    => "low,moderate,normal,high,critical" (Required)
-    #
-    # :vulnerabilities => An array of Nexpose vuln IDs. This is NOT the same as vuln ID.  (Required)
-    # :comments        => An array of comments to accompany this ticket
-    #
-    # @return The ticket ID if the ticket creation was successful, {@code false} otherwise
-    #
-    def create_ticket(ticket_info)
-      ticket_name = ticket_info[:name]
-      unless ticket_name
-        raise ArgumentError.new 'Ticket name is required'
-      end
-
-      device_id = ticket_info[:device_id]
-      unless device_id
-        raise ArgumentError.new 'Device ID is required'
-      end
-
-      assigned_to = ticket_info[:assigned_to]
-      unless assigned_to
-        raise ArgumentError.new 'Assignee name is required'
-      end
-
-      priority = ticket_info[:priority]
-      unless priority
-        raise ArgumentError.new 'Ticket priority is required'
-      end
-
-      vulnerabilities = ticket_info[:vulnerabilities]
-      if not vulnerabilities or vulnerabilities.count < 1
-        raise ArgumentError.new 'Vulnerabilities are required'
-      end
-
-      comments = ticket_info[:comments]
-      base_xml = make_xml 'TicketCreateRequest'
-
-      required_attributes = {
-          'name' => ticket_name,
-          'priority' => priority,
-          'device-id' => device_id,
-          'assigned-to' => assigned_to
-      }
-
-      create_request_xml = REXML::Element.new 'TicketCreate'
-      create_request_xml.add_attributes required_attributes
-
-      # Add vulnerabilities
-      vulnerabilities_xml = REXML::Element.new 'Vulnerabilities'
-      vulnerabilities.each do |vuln_id|
-        vulnerabilities_xml.add_element 'Vulnerability', {'id' => vuln_id.downcase}
-      end
-      create_request_xml.add_element vulnerabilities_xml
-
-      # Add comments
-      if comments and comments.count > 0
-        comments_xml = REXML::Element.new 'Comments'
-        comments.each do |comment|
-          comment_xml = REXML::Element.new 'Comment'
-          comment_xml.add_text comment
-          comments_xml.add_element comment_xml
-        end
-
-        create_request_xml.add_element comments_xml
-      end
-
-      base_xml.add_element create_request_xml
-      r = execute base_xml, '1.2'
-      if r.success
-        r.res.elements.each('TicketCreateResponse') do |group|
-          return group.attributes['id'].to_i
-        end
-      else
-        false
-      end
     end
   end
 
@@ -179,21 +75,181 @@ module Nexpose
     # The current status of the ticket.
     attr_accessor :state
 
-    def initialize(id, name)
+    def initialize(name, id)
       @id, @name = id, name
     end
 
     def self.parse(xml)
-      ticket = new(xml.attributes['id'].to_i,
-                   xml.attributes['name'])
+      ticket = new(xml.attributes['name'],
+                   xml.attributes['id'].to_i)
       ticket.device_id = xml.attributes['device-id'].to_i
       ticket.assigned_to = xml.attributes['assigned-to']
-      ticket.priority = xml.attributes['priority']
+      lookup = Ticket::Priority.constants.reduce({}) { |a, e| a[Ticket::Priority.const_get(e)] = e; a }
+      ticket.priority = lookup[xml.attributes['priority']]
       ticket.author = xml.attributes['author']
       ticket.created_on = DateTime::parse(xml.attributes['created-on'])
       lookup = Ticket::State.constants.reduce({}) { |a, e| a[Ticket::State.const_get(e)] = e; a }
       ticket.state = lookup[xml.attributes['state']]
       ticket
+    end
+
+    module State
+      OPEN = 'O'
+      ASSIGNED = 'A'
+      MODIFIED = 'M'
+      FIXED = 'X'
+      PARTIAL = 'P'
+      REJECTED_FIX = 'R'
+      PRIORITIZED = 'Z'
+      NOT_REPRODUCIBLE = 'F'
+      NOT_ISSUE = 'I'
+      CLOSED = 'C'
+      UNKNOWN = 'U'
+    end
+
+    module Priority
+      LOW = 'low'
+      MODERATE = 'moderate'
+      NORMAL = 'normal'
+      HIGH = 'high'
+      CRITICAL = 'critical'
+    end
+  end
+  
+  class Ticket < TicketSummary
+    
+    # List of vulnerabilities (by ID) this ticket addresses.
+    attr_accessor :vulnerabilities
+
+    # Array of comments about the ticket.
+    attr_accessor :comments
+
+    # History of events on this ticket.
+    attr_accessor :history
+
+    def initialize(name, id = nil)
+      @id, @name = id, name
+      @priority = Priority::NORMAL
+      @vulnerabilities = []
+      @comments = []
+      @history = []
+    end
+
+    # Save this ticket to a Nexpose console.
+    #
+    # @param [Connection] connection Connection to console where ticket exists.
+    # @return [Fixnum] Unique ticket ID assigned to this ticket.
+    #
+    def save(connection)
+      xml = connection.make_xml('TicketCreateRequest')
+      xml.add_element(to_xml)
+      puts xml.to_s
+
+      response = connection.execute(xml, '1.2')
+      @id = response.attributes['id'].to_i if response.success
+    end
+
+    # Delete this ticket from the system.
+    #
+    # @param [Connection] connection Connection to console where ticket exists.
+    # @return [Boolean] Whether the ticket was successfully delete.
+    #
+    def delete(connection)
+      connection.delete_ticket(@id)
+    end
+
+    # Load existing ticket data.
+    #
+    # @param [Connection] connection Connection to console where ticket exists.
+    # @param [Fixnum] id Ticket ID of an existing ticket.
+    # @return [Ticket] Ticket populated with current state.
+    #
+    def self.load(connection, id)
+      # TODO Load multiple tickets in a single request, as supported by API.
+      xml = connection.make_xml('TicketDetailsRequest')
+      xml.add_element('Ticket', {'id' => id})
+      response = connection.execute(xml, '1.2')
+      response.res.elements.each('//TicketInfo') do |info|
+        return parse_details(info)
+      end
+    end
+
+    def to_xml
+      xml = REXML::Element.new('TicketCreate')
+      xml.add_attributes({ 'name' => @name,
+                           'priority' => @priority,
+                           'device-id' => @device_id,
+                           'assigned-to' => @assigned_to })
+
+      vuln_xml = REXML::Element.new('Vulnerabilities')
+      @vulnerabilities.each do |vuln_id|
+        vuln_xml.add_element('Vulnerability', { 'id' => vuln_id.downcase })
+      end
+      xml.add_element(vuln_xml)
+
+      unless @comments.empty?
+        comments_xml = REXML::Element.new('Comments')
+        @comments.each do |comment|
+          comment_xml = REXML::Element.new('Comment')
+          comment_xml.add_text(comment)
+          comments_xml.add_element(comment_xml)
+        end
+        xml.add_element(comments_xml)
+      end
+
+      xml
+    end
+
+    def self.parse_details(xml)
+      ticket = parse(xml)
+
+      xml.elements.each('Vulnerabilities/Vulnerability') do |vuln|
+        ticket.vulnerabilities << vuln.attributes['id']
+      end
+
+      xml.elements.each('TicketHistory/Entry') do |entry|
+        ticket.history << Event.parse(entry)
+      end
+
+      ticket.comments = ticket.history.select { |h| h.description == 'Added comment' }.map { |e| e.comment }
+
+      ticket
+    end
+
+    class Event
+
+      # Date and time of the ticket event.
+      attr_reader :created_on
+      # The login name of the person responsible for the event.
+      attr_reader :author
+      # The status of the ticket at the time the event was recorded.
+      attr_reader :state
+      # Description of the ticket event.
+      attr_accessor :description
+      # Comment on the ticket event.
+      attr_accessor :comment
+
+      def initialize(state, author, created)
+        @state, @author, @created = state, author, created
+      end
+
+      def self.parse(xml)
+        author = xml.attributes['author']
+        created_on = DateTime::parse(xml.attributes['created-on'])
+
+        event = REXML::XPath.first(xml, 'Event')
+        lookup = Ticket::State.constants.reduce({}) { |a, e| a[Ticket::State.const_get(e)] = e; a }
+        state = lookup[event.attributes['state']]
+        desc = event.text
+
+        event = new(state, author, created_on)
+
+        comment = REXML::XPath.first(xml, 'Comment')
+        event.comment = comment.text if comment
+        
+        event.description = desc if desc
+        event
+      end
     end
   end
 end
