5b15f05c90c6556c18bc16c2039ea30879fdd4f6
Michael S. Daines <michael_daines@rapid7.com>
2013-08-16 06:55:03 -0700

    Incremental work on VulnException save.

--- lib/nexpose/vuln_exception.rb
+++ lib/nexpose/vuln_exception.rb
@@ -2,52 +2,28 @@ module Nexpose
   module NexposeAPI
     include XMLUtils
 
-    #-----------------------------------------------------------------------
-    # Returns an array of vulnerability exceptions and their associated
-    # attributes.
+    # Retrieve vulnerability exceptions.
     #
-    # @param status - (optional) The status of the vulnerability exception:
-    # "Under Review", "Approved", "Rejected"
-    #-----------------------------------------------------------------------
-    def list_vuln_exceptions(status = nil)
+    # @param [String] status Filter exceptions by the current status.
+    #   @see Nexpose::VulnException::Status
+    # @param [String] duration A time interval in the format "PnYnMnDTnHnMnS".
+    # @return [Array[VulnException]] List of matching vulnerability exceptions.
+    #
+    def list_vuln_exceptions(status = nil, duration = nil)
       option = {}
-
-      if status && !status.empty?
-        if status =~ /Under Review|Approved|Rejected/
-          option['status'] = status
-        else
-          raise ArgumentError.new 'The vulnerability status passed in is invalid!'
-        end
-      end
-
+      option['status'] = status if status
+      option['time-duration'] = duration if duration
       xml = make_xml('VulnerabilityExceptionListingRequest', option)
-      r = execute xml, '1.2'
+      response = execute(xml, '1.2')
 
-      if r.success
-        res = []
-        r.res.elements.each('//VulnerabilityException') do |ve|
-          submitter_comment = ve.elements['submitter-comment']
-          reviewer_comment = ve.elements['reviewer-comment']
-          res << {
-            :vuln_id => ve.attributes['vuln-id'],
-            :exception_id => ve.attributes['exception-id'],
-            :submitter => ve.attributes['submitter'],
-            :reviewer => ve.attributes['reviewer'],
-            :status => ve.attributes['status'],
-            :reason => ve.attributes['reason'],
-            :scope => ve.attributes['scope'],
-            :device_id => ve.attributes['device-id'],
-            :port_no => ve.attributes['port-no'],
-            :expiration_date => ve.attributes['expiration-date'],
-            :vuln_key => ve.attributes['vuln-key'],
-            :submitter_comment => submitter_comment.nil? ? '' : submitter_comment.text,
-            :reviewer_comment => reviewer_comment.nil? ? '' : reviewer_comment.text
-          }
+      xs = []
+      if response.success
+        response.res.elements.each('//VulnerabilityException') do |ve|
+          xs << VulnException.parse(ve)
         end
         res
-      else
-        false
       end
+      xs
     end
 
     alias_method :vuln_exceptions, :list_vuln_exceptions
@@ -69,16 +45,6 @@ module Nexpose
     #-------------------------------------------------------------------------------------------------------------------
     def vuln_exception_create(input)
       options = {}
-
-      if input.nil?
-        raise ArgumentError.new 'The input element cannot be null'
-      end
-
-      vuln_id = input[:vuln_id]
-      unless vuln_id
-        raise ArgumentError.new 'The vulnerability ID is required'
-      end
-      options['vuln-id'] = vuln_id
 
       reason = input[:reason]
       if reason.nil? || reason.empty?
@@ -322,6 +288,162 @@ module Nexpose
       xml = make_xml('VulnerabilityExceptionDeleteRequest', {'exception-id' => exception_id})
       r = execute xml, '1.2'
       r.success
+    end
+  end
+
+  # In addition to attributes listed as required in the preceding table, certain
+  # attributes are necessary for certain exception scopes, even though they are
+  # listed as optional.
+  # • An exception for all instances of a vulnerability on all assets only
+  #   requires the vuln_id attribute. The device_id, vuln_key and port
+  #   attributes are ignored for this scope type.
+  # • An exception for all instances on a specific asset requires the vuln_id
+  #   and device_id attributes. The vuln_key and port attributes are ignored for
+  #   this scope type.
+  # • An exception for a specific instance of a vulnerability on a specific
+  #   asset requires the vuln_id, device_id. Additionally, the port and/or the
+  #   key attribute must be specified.
+  #
+  class VulnException
+
+    # Unique identifier assigned to an exception.
+    attr_accessor :id
+    # Unique identifier of a vulnerability.
+    attr_accessor :vuln_id
+    # The name of submitter of the exception.
+    attr_accessor :submitter
+    # The name of the reviewer of the exception.
+    attr_accessor :reviewer
+    # The state of the exception in the work flow process.
+    # @see Nexpose::VulnException::Status
+    attr_accessor :status
+    # The reason for the exception status.
+    # @see Nexpose::VulnException::Reason
+    attr_accessor :reason
+    # The scope of the exception.
+    # @see Nexpose::VulnException::Scope
+    attr_accessor :scope
+    # ID of device, if this exception applies to only one device.
+    attr_accessor :device_id
+    # Port on a device, if this exception applies to a specific port.
+    attr_accessor :port
+    # The specific vulnerable component in a discovered instance of the
+    # vulnerability referenced by the vuln_id, such as a program, file or user
+    # account.
+    attr_accessor :vuln_key
+    # The date an exception will expire, causing the vulnerability to be
+    # included in report risk scores.
+    attr_accessor :expiration
+    # Any comment provided by the submitter.
+    attr_accessor :submitter_comment
+    # Any comment provided by the reviewer.
+    attr_accessor :reviewer_comment
+
+    def initialize(vuln_id, scope, reason, status = nil)
+      @vuln_id, @scope, @reason, @status = vuln_id, scope, reason, status
+    end
+
+    # Create this exception on the security console.
+    #
+    # @param [Connection] connection Connection to security console.
+    #
+    def save(connection, comment = nil)
+      validate
+      @submitter_comment = comment if comment
+      response = execute(to_xml, '1.2')
+    end
+
+    def to_xml
+      xml = connection.make_xml('VulnerabilityExceptionCreateRequest')
+      xml.add_attributes({ 'vuln-id' => @vuln_id,
+                           'scope' => @scope,
+                           'reason' => @reason }
+      case @scope
+      when Scope::ALL_INSTANCES_ON_A_SPECIFIC_ASSET
+        xml.add_attributes({ 'device-id' => @device_id })
+      when Scope::SPECIFIC_INSTANCE_OF_SPECIFIC_ASSET
+        xml.add_attributes({ 'device-id' => @device_id,
+                             'port-no' => @port,
+                             'vuln-key' => @vuln_key })
+      end
+
+      if @submitter_comment
+        comment = REXML::Element.new('submitter-comment')
+        comment.add_text(@submitter_comment)
+        xml.add_element(comment)
+      end
+
+      xml
+    end
+
+    # Validate that this exception meets to requires for the assigned scope.
+    #
+    def validate
+      raise ArgumentError.new('No vuln_id.') unless @vuln_id
+      raise ArgumentError.new('No scope.') unless @scope
+      raise ArgumentError.new('No reason.') unless @reason
+
+      case @scope
+      when Scope::ALL_INSTANCES
+        @device_id = @port = @vuln_key = nil
+      when Scope::ALL_INSTANCES_ON_A_SPECIFIC_ASSET
+        raise ArgumentError.new('No device_id.') unless @device_id
+        @port = @vuln_key = nil
+      when Scope::SPECIFIC_INSTANCE_OF_SPECIFIC_ASSET
+        raise ArgumentError.new('No device_id.') unless @device_id
+        raise ArgumentError.new('Port or vuln_key is required.') unless @port or @vuln_key
+      else
+        raise ArgumentError.new("Invalid scope: #{@scope}")
+      end
+    end
+
+    def self.parse(xml)
+      exception = new(xml.attributes['vuln-id'],
+                      xml.attributes['scope'],
+                      xml.attributes['reason'],
+                      xml.attributes['status'])
+
+      exception.id = xml.attributes['exception-id']
+      exception.submitter = xml.attributes['submitter']
+      exception.reviewer = xml.attributes['reviewer']
+      exception.device_id = xml.attributes['device-id']
+      exception.port = xml.attributes['port-no']
+      exception.vuln_key = xml.attributes['vuln-key']
+      # TODO Convert to Date/Time object.
+      exception.expiration = xml.attributes['expiration-date']
+
+      submitter_comment = xml.elements['submitter-comment']
+      exception.submitter_comment = submitter_comment.text if submitter_comment
+      reviewer_comment = xml.elements['reviewer-comment']
+      exception.reviewer_comment = reviewer_comment.text if reviewer_comment
+
+      exception
+    end
+
+    # The state of a vulnerability exception in the work flow process.
+    #
+    module Status
+      UNDER_REVIEW = 'Under Review'
+      APPROVED = 'Approved'
+      REJECTED = 'Rejected'
+    end
+
+    # The reason for the exception status.
+    #
+    module Reason
+      FALSE_POSITIVE = 'False Positive'
+      COMPENSATING_CONTROL = 'Compensating Control'
+      ACCEPTABLE_USE = 'Acceptable Use'
+      ACCEPTABLE_RISK = 'Acceptable Risk'
+      OTHER = 'Other'
+    end
+
+    # The scope of the exception.
+    #
+    module Scope
+      ALL_INSTANCES = 'All Instances'
+      ALL_INSTANCES_ON_A_SPECIFIC_ASSET = 'All Instances on a Specific Asset'
+      SPECIFIC_INSTANCE_OF_SPECIFIC_ASSET = 'Specific Instance of Specific Asset'
     end
   end
 end
