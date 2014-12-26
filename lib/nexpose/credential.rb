8aa44a0171c9d2d404b6bcc0d69ebfce4b5e5141
Scott Green <sgreen@rapid7.com>
2014-12-26 13:47:46 -0800

    removing creds file, renaming to credential

---
+++ lib/nexpose/credential.rb
@@ -0,0 +1,10 @@
+module Nexpose
+
+  # Contains the shared methods for the SiteCredential and SharedCredential Objects.
+  # See Nexpose::SiteCredential or Nexpose::SharedCredential for additional info.
+  class Credential
+
+  end
+
+
+end
