7b2f2fcbd75fbc2d6a7f7131cc03d8c789b440a5
Erran Carey <me@errancarey.com>
2015-02-12 21:38:49 -0600

    Fix CodeClimate coverage generation
    * Load codeclimate-test-reporter before nexpose.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,4 @@
-require 'nexpose'
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
 
-if ENV['CODECLIMATE_REPO_TOKEN']
-  require 'codeclimate-test-reporter'
-  CodeClimate::TestReporter.start
-end
+require 'nexpose'
