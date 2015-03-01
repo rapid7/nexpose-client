265af228874c8540e553caedfb57a32e8b30e594
Erran Carey <me@errancarey.com>
2015-02-28 22:56:42 -0600

    Update keys for censored values

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'codeclimate-test-reporter'
 require 'simplecov'
 require 'vcr'
+require_relative './matchers'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   SimpleCov::Formatter::HTMLFormatter,
@@ -9,21 +10,11 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
 
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/cassettes'
-  config.filter_sensitive_data('<NEXPOSE_CONSOLE_HOSTNAME>') { ENV['NEXPOSE_HOSTNAME'] }
-  config.filter_sensitive_data('<NEXPOSE_CONSOLE_USERNAME>') { ENV['NEXPOSE_USERNAME'] }
-  config.filter_sensitive_data('<NEXPOSE_CONSOLE_PASSWORD>') { ENV['NEXPOSE_PASSWORD'] }
+  config.filter_sensitive_data('nexpose.local') { ENV['NEXPOSE_HOSTNAME'] }
+  config.filter_sensitive_data('johndoe') { ENV['NEXPOSE_USERNAME'] }
+  config.filter_sensitive_data('password123') { ENV['NEXPOSE_PASSWORD'] }
   config.hook_into :webmock
 end
-
-RSpec.configure do |config|
-  config.before(:example, :vcr) do
-    unless ENV['NEXPOSE_HOSTNAME'] && ENV['NEXPOSE_USERNAME'] && ENV['NEXPOSE_PASSWORD']
-      raise ArgumentError, 'You must set the NEXPOSE_HOSTNAME, NEXPOSE_USERNAME, and NEXPOSE_PASSWORD environment variables.'
-    end
-  end
-end
-
-require_relative './matchers'
 
 SimpleCov.start
 
