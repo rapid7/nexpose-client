40c725e1db81ede220b7a79d5a651b9887c98dae
Gavin Schneider <gavin_schneider@rapid7.com>
2015-03-17 15:45:49 -0700

    Update rspec tests for site to use include_asset

--- spec/nexpose/site_spec.rb
+++ spec/nexpose/site_spec.rb
@@ -5,54 +5,54 @@ describe Nexpose::Site do
   let(:site_name) { 'joe blow site' }
   subject { Nexpose::Site.new(site_name, scan_template) }
 
-  describe '#add_asset' do
+  describe '#include_asset' do
     context 'with a hostname' do
       it 'adds a new HostName to the assets list' do
-        subject.add_asset('nexpose.local')
+        subject.include_asset('nexpose.local')
 
-        expect(subject.assets).to include(Nexpose::HostName.new('nexpose.local'))
+        expect(subject.included_scan_targets[:addresses]).to include(Nexpose::HostName.new('nexpose.local'))
       end
     end
 
     context 'with a single IP address' do
       it 'adds a new IPRange to the assets list' do
-        subject.add_asset('192.168.1.1')
+        subject.include_asset('192.168.1.1')
 
-        expect(subject.assets).to include(Nexpose::IPRange.new('192.168.1.1'))
+        expect(subject.included_scan_targets[:addresses]).to include(Nexpose::IPRange.new('192.168.1.1'))
       end
     end
 
     context 'with a multiple IP address' do
       it 'adds a new IPRange to the assets list' do
-        subject.add_asset('192.168.1.0/24')
+        subject.include_asset('192.168.1.0/24')
 
-        expect(subject.assets).to include(Nexpose::IPRange.new('192.168.1.0', '192.168.1.255'))
+        expect(subject.included_scan_targets[:addresses]).to include(Nexpose::IPRange.new('192.168.1.0', '192.168.1.255'))
       end
     end
   end
 
   describe '#add_host' do
     it 'adds a new HostName to the assets list' do
-      subject.add_asset('nexpose.local')
+      subject.include_asset('nexpose.local')
 
-      expect(subject.assets).to include(Nexpose::HostName.new('nexpose.local'))
+      expect(subject.included_scan_targets[:addresses]).to include(Nexpose::HostName.new('nexpose.local'))
     end
   end
 
   describe '#add_ip' do
     context 'with a single IP address' do
       it 'adds a new IPRange to the assets list' do
-        subject.add_asset('192.168.1.1')
+        subject.include_asset('192.168.1.1')
 
-        expect(subject.assets).to include(Nexpose::IPRange.new('192.168.1.1'))
+        expect(subject.included_scan_targets[:addresses]).to include(Nexpose::IPRange.new('192.168.1.1'))
       end
     end
 
     context 'with a multiple IP address' do
       it 'adds a new IPRange to the assets list' do
-        subject.add_asset('192.168.1.0/24')
+        subject.include_asset('192.168.1.0/24')
 
-        expect(subject.assets).to include(Nexpose::IPRange.new('192.168.1.0', '192.168.1.255'))
+        expect(subject.included_scan_targets[:addresses]).to include(Nexpose::IPRange.new('192.168.1.0', '192.168.1.255'))
       end
     end
   end
