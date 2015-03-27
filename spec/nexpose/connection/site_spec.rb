514f8e02a9cf3281a14141fa291d093b2b219162
Erran Carey <me@errancarey.com>
2015-03-27 15:34:12 -0500

    Use Site#included_addresses over Site#included_scan_targets[:addresses]

--- spec/nexpose/connection/site_spec.rb
+++ spec/nexpose/connection/site_spec.rb
@@ -18,7 +18,7 @@ describe Nexpose::Connection, :with_api_login do
       let(:basic_site) do
         Nexpose::Site.new('test site name 1').tap do |site|
           site.description = 'test site description 1'
-          site.included_scan_targets[:addresses] = ['localhost']
+          site.included_addresses = %w(localhost)
 
           VCR.use_cassette('basic_site') do
             site.save(connection)
