e945ea35d45746622f272cb8c185e7cfcc7ed7e7
Jon Hart <jon_hart@rapid7.com>
2015-08-21 17:18:10 -0700

    Add spec for scan template

--- spec/nexpose/scan_template_spec.rb
+++ spec/nexpose/scan_template_spec.rb
@@ -1,18 +1,78 @@
 require 'spec_helper'
+require 'securerandom'
 
 describe Nexpose::ScanTemplate do
-  let(:template) { Nexpose::ScanTemplate.new(SecureRandom.hex(16)) }
+  let(:random_things) { 0.upto(1+rand(10)).to_a.map { |t| SecureRandom.hex(8) } }
+  subject { described_class.new(IO.read(File.join(%w(spec data example_template.xml)))) }
 
-  describe '#vuln_checks' do
+  context 'vulnerability checks' do
     context 'by default' do
-      it 'returns no enabled vuln categories' do
-        expect(template.checks_by_category).to be_empty
+      it 'returns no enabled vulnerability checks' do
+        expect(subject.vuln_checks).to be_empty
       end
-      it 'returns no enabled vuln types' do
-        expect(template.checks_by_type).to be_empty
+    end
+    context 'when checks are enabled' do
+      it 'returns enabled vulnerability checks' do
+        random_things.each do |thing|
+          subject.enable_vuln_check(thing)
+        end
+        expect(subject.vuln_checks).to eq(random_things)
       end
-      it 'returns no enabled vuln checks' do
-        expect(template.vuln_checks).to be_empty
+    end
+    context 'when checks are disabled' do
+      skip 'returns disabled vulnerability checks' do
+        random_things.each do |thing|
+          subject.disable_vuln_check(thing)
+        end
+        expect(subject.vuln_checks).to eq(random_things)
+      end
+    end
+  end
+
+  context 'vulnerability categories' do
+    context 'by default' do
+      it 'returns no enabled vulnerability categories' do
+        expect(subject.checks_by_category).to be_empty
+      end
+    end
+    context 'when categories are enabled' do
+      it 'returns enabled vulnerability categories' do
+        random_things.each do |thing|
+          subject.enable_checks_by_category(thing)
+        end
+        expect(subject.checks_by_category).to eq(random_things)
+      end
+    end
+    context 'when categories are disabled' do
+      skip 'returns disabled vulnerability categories' do
+        random_things.each do |thing|
+          subject.disable_checks_by_category(thing)
+        end
+        expect(subject.checks_by_category).to eq(random_things)
+      end
+    end
+  end
+
+  context 'vulnerability types' do
+    context 'by default' do
+      it 'returns no enabled vulnerability types' do
+        expect(subject.checks_by_type).to be_empty
+      end
+    end
+    context 'when types are enabled' do
+      it 'returns enabled vulnerability types' do
+        random_things.each do |thing|
+          subject.enable_checks_by_type(thing)
+        end
+        expect(subject.checks_by_type).to eq(random_things)
+      end
+    end
+    context 'when types are disabled' do
+      skip 'returns disabled vulnerability types' do
+        random_things.each do |thing|
+          subject.disable_checks_by_type(thing)
+        end
+        expect(subject.checks_by_type).to eq(random_things)
       end
     end
   end
