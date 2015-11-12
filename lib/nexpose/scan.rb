8b01094e0d10472a7684c00cbee63d0951a3c6f3
Gavin Schneider <gavin_schneider@rapid7.com>
2015-11-12 15:56:36 -0800

    Add methods to retrieve paused scans
    Two separate methods for site scans and all scans using two different
    data table endpoints.
    
    Added new class ActiveScan based on CompletedScan to handle parsing the
    data differently.

--- lib/nexpose/scan.rb
+++ lib/nexpose/scan.rb
@@ -357,6 +357,31 @@ module Nexpose
       end
     end
 
+    # Get paused scans for a site.
+    #
+    # @param [Fixnum] site_id Site ID to retrieve paused scans for.
+    # @param [Fixnum] limit The maximum number of records to return from this call.
+    # @return [ActiveScan] The paused scan details.
+    #
+    def paused_scans(site_id = nil, limit = nil)
+      uri = "/data/scan/site/#{site_id}?status=active"
+      rows = AJAX.row_pref_of(limit)
+      params = { 'sort' => 'endTime', 'dir' => 'DESC', 'startIndex' => 0 }
+      AJAX.preserving_preference(self, 'site-active-scans') do
+        data = DataTable._get_json_table(self, uri, params, rows, limit).select { |scan| scan['paused']}
+        data.map(&ActiveScan.method(:parse_json))
+      end
+    end
+
+    # Get paused scans for all sites.
+    #
+    # @return [ActiveScan] The paused scan details.
+    #
+    def paused_scans
+      uri = '/data/site/scans/dyntable.xml?printDocType=0&tableID=siteScansTable&activeOnly=true'
+      data = DataTable._get_dyn_table(self, uri).select { |scan| (scan['Status'].include? 'Paused')}
+    end
+
     # Export the data associated with a single scan, and optionally store it in
     # a zip-compressed file under the provided name.
     #
@@ -763,6 +788,42 @@ module Nexpose
         :stopped
       when 'A'
         :aborted
+      when 'P'
+        :paused
+      when 'I'
+        :integrating
+      else
+        :unknown
+      end
+    end
+  end
+
+  class ActiveScan < CompletedScan
+    def self.parse_dyntable(json)
+      new do
+        @id = json['Scan ID']
+        @site_id = json['Site ID']
+        @status = CompletedScan._parse_status(json['Status Code'])
+        @start_time = Time.at(json['Started'].to_i / 1000)
+        @end_time = Time.at(json['Progress'].to_i / 1000)
+        @duration = json['Elapsed'].to_i
+        @vulns = json['Vulnerabilities Discovered'].to_i
+        @assets = json['Devices Discovered'].to_i
+        @risk_score = json['riskScore']
+        @type = json['Scan Type'] == 'Manual' ? :manual : :scheduled
+        @engine_name = json['Scan Engine']
+      end
+    end
+
+    # Internal method to parsing status codes.
+    def self._parse_status(code)
+      case code
+      when 'U'
+        :running
+      when 'P'
+        :paused
+      when 'I'
+        :integrating
       else
         :unknown
       end
