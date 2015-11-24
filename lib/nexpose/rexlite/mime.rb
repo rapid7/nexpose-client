cb4cd1fd03abf711210dcb4d3e6c1d278472fbaf
Gavin Schneider <gavin_schneider@rapid7.com>
2015-11-23 16:38:41 -0800

    Re-indent Rexlite::MIME files

--- lib/nexpose/rexlite/mime.rb
+++ lib/nexpose/rexlite/mime.rb
@@ -1,10 +1,10 @@
 # -*- coding: binary -*-
 module Rexlite
-module MIME
+  module MIME
 
-require 'nexpose/rexlite/mime/header'
-require 'nexpose/rexlite/mime/part'
-require 'nexpose/rexlite/mime/message'
+    require 'nexpose/rexlite/mime/header'
+    require 'nexpose/rexlite/mime/part'
+    require 'nexpose/rexlite/mime/message'
 
-end
+  end
 end
