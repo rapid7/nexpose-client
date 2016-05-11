e92299daaa674b4d4168e8c19df43e9e5fe5693b
sgreen-r7 <sgreen@rapid7.com>
2016-05-11 16:22:20 -0700

    double the REXML::Security.entity_expansion_text_limit to allow for parsing of large vuln content

--- lib/nexpose.rb
+++ lib/nexpose.rb
@@ -110,6 +110,10 @@ require 'nexpose/maint'
 require 'nexpose/version'
 require 'nexpose/wait'
 
+# Double the size of the default limit,
+# to work around large vuln content.
+REXML::Security.entity_expansion_text_limit = 20_000
+
 module Nexpose
 
   # Echos the last XML API request and response for the specified object.  (Useful for debugging)
