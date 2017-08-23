bffcc6ac5d0019c5bd30e7b6eff761fe2236ade5
sgreen-r7 <sgreen@rapid7.com>
2017-08-22 17:24:41 -0700

    including the credential_helper file might be helpful...

---
+++ lib/nexpose/credential_helper.rb
@@ -0,0 +1,167 @@
+module Nexpose
+  module CredentialHelper
+
+    # sets the Concurrent Versioning System (CVS) service
+    def set_cvs_service(username = nil, password = nil)
+      self.user_name = username
+      self.password  = password
+      self.service   = Service::CVS
+    end
+
+    # sets the DB2 service
+    def set_db2_service(database = nil, username = nil, password = nil)
+      self.database  = database
+      self.user_name = username
+      self.password  = password
+      self.service   = Service::DB2
+    end
+
+    # sets the File Transfer Protocol (FTP) service
+    def set_ftp_service(username = nil, password = nil)
+      self.user_name = username
+      self.password  = password
+      self.service   = Service::FTP
+    end
+
+    # sets the IBM AS/400 service.
+    def set_as400_service(domain = nil, username = nil, password = nil)
+      self.domain    = domain
+      self.user_name = username
+      self.password  = password
+      self.service   = Service::AS400
+    end
+
+    # sets the Lotus Notes/Domino service.
+    def set_notes_service(password = nil)
+      self.notes_id_password = password
+      self.service           = Service::NOTES
+    end
+
+    # sets the Microsoft SQL Server service.
+    def set_tds_service(database = nil, domain = nil, username = nil, password = nil)
+      self.database         = database
+      self.domain           = domain
+      self.use_windows_auth = domain.nil?
+      self.user_name        = username
+      self.password         = password
+      self.service          = Service::TDS
+    end
+
+    # sets the Microsoft Windows/Samba (SMB/CIFS) service.
+    def set_cifs_service(domain = nil, username = nil, password = nil)
+      self.domain    = domain
+      self.user_name = username
+      self.password  = password
+      self.service   = Service::CIFS
+    end
+
+    # sets the Microsoft Windows/Samba LM/NTLM Hash (SMB/CIFS) service.
+    def set_cifshash_service(domain = nil, username = nil, password = nil)
+      self.domain    = domain
+      self.user_name = username
+      self.password  = password
+      self.service   = Service::CIFSHASH
+    end
+
+    # sets the MySQL Server service.
+    def set_mysql_service(database = nil, username = nil, password = nil)
+      self.database  = database
+      self.user_name = username
+      self.password  = password
+      self.service   = Service::MYSQL
+    end
+
+    # sets the Oracle service.
+    def set_oracle_service(sid = nil, username = nil, password = nil)
+      self.database  = sid
+      self.user_name = username
+      self.password  = password
+      self.service   = Service::ORACLE
+    end
+
+    # sets the Post Office Protocol (POP) service.
+    def set_pop_service(username = nil, password = nil)
+      self.user_name = username
+      self.password  = password
+      self.service   = Service::POP
+    end
+
+    # sets the PostgreSQL service.
+    def set_postgresql_service(database = nil, username = nil, password = nil)
+      self.database  = database
+      self.user_name = username
+      self.password  = password
+      self.service   = Service::POSTGRESQL
+    end
+
+    # sets the Remote Execution service.
+    def set_remote_execution_service(username = nil, password = nil)
+      self.user_name = username
+      self.password  = password
+      self.service   = Service::REMOTE_EXECUTION
+    end
+
+    # sets the Secure Shell (SSH) service.
+    def set_ssh_service(username = nil, password = nil, elevation_type = nil, elevation_user = nil, elevation_password = nil)
+      self.user_name                     = username
+      self.password                      = password
+      self.permission_elevation_type     = elevation_type || ElevationType::NONE
+      self.permission_elevation_user     = elevation_user
+      self.permission_elevation_password = elevation_password
+      self.service                       = Service::SSH
+    end
+
+    # sets the Secure Shell (SSH) Public Key service.
+    def set_ssh_key_service(username, pemkey,  password = nil, elevation_type = nil, elevation_user = nil, elevation_password = nil)
+      self.user_name                     = username
+      self.password                      = password
+      self.pem_format_private_key        = pemkey
+      self.permission_elevation_type     = elevation_type || ElevationType::NONE
+      self.permission_elevation_user     = elevation_user
+      self.permission_elevation_password = elevation_password
+      self.service                       = Service::SSH_KEY
+    end
+
+    # sets the Simple Network Management Protocol v1/v2c service.
+    def set_snmp_service(community_name = nil)
+      self.community_name = community_name
+      self.service        = Service::SNMP
+    end
+
+    # sets the Simple Network Management Protocol v3 service.
+    def set_snmpv3_service(authentication_type = AuthenticationType::NOAUTH, username = nil, password = nil, privacy_type = PrivacyType::NOPRIV, privacy_password = nil)
+      self.authentication_type = authentication_type
+      self.user_name           = username
+      self.password            = password
+      self.privacy_type        = privacy_type
+      self.privacy_password    = privacy_password
+      self.service             = Service::SNMPV3
+    end
+
+    # sets the Sybase SQL Server service.
+    def set_sybase_service(database = nil, domain = nil, username = nil, password = nil)
+      self.database         = database
+      self.domain           = domain
+      self.use_windows_auth = domain.nil?
+      self.user_name        = username
+      self.password         = password
+      self.service          = Service::SYBASE
+    end
+
+    # sets the Telnet service.
+    def set_telnet_service(username = nil, password = nil)
+      self.user_name = username
+      self.password  = password
+      self.service   = Service::TELNET
+    end
+
+    # sets the Web Site HTTP Authentication service.
+    def set_http_service(domain = nil, username = nil, password = nil)
+      self.domain    = domain
+      self.user_name = username
+      self.password  = password
+      self.service   = Service::HTTP
+    end
+
+  end
+end
